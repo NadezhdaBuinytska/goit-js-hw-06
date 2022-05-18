80aa490707f4b3707aadc80589ca6789aca94612
NadezhdaBuinytska <nadezhda_bat@ukr.net>
2022-05-18 19:37:24 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,21 @@
 "use strict";
 
-const totalCategories = document.querySelectorAll(".item");
+const totalCategories = document.querySelectorAll("#categories .item");
 console.log(`Number of categories: ${totalCategories.length}`);
 
-const mapCategories = [...totalCategories]
-  .map(
-    categories => `Category: ${categories.children[0].textContent}
-Elements: ${categories.children[1].children.length}`
-  )
-  .join("\n");
-console.log(mapCategories);
+// const mapCategories = [...totalCategories]
+//   .flatMap(
+//     categories =>
+//     `Category: ${categories.children[0].textContent}
+// Elements: ${categories.children[1].children.length}`
+// )
+// .join("\n");
+// console.log(mapCategories);
+
+totalCategories.forEach(item => {
+  const titleCategoryEl = item.querySelector("h2");
+  const itemsCategoryEl = item.querySelectorAll("li");
+
+  console.log(`Category: ${ titleCategoryEl.textContent }`);
+  console.log(`Elements: ${itemsCategoryEl.length}`);
+})
